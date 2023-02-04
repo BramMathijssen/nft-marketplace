715c62e5670109f312ad9fdb7956323ad33881e6
Bram <Bram.mathijssen@gmail.com>
2023-02-04 13:59:19 +0100

    init next-js frontend for marketplace

--- subgraph/src/nft-marketplace.ts
+++ subgraph/src/nft-marketplace.ts
@@ -1,3 +1,4 @@
+import {BigInt, Address} from "@graphprotocol/graph-ts";
 import {
   ItemBought as ItemBoughtEvent,
   ItemCanceled as ItemCanceledEvent,
@@ -6,48 +7,13 @@ import {
 import { ItemBought, ItemCanceled, ItemListed } from "../generated/schema"
 
 export function handleItemBought(event: ItemBoughtEvent): void {
-  let entity = new ItemBought(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.buyer = event.params.buyer
-  entity.nftAddress = event.params.nftAddress
-  entity.tokenId = event.params.tokenId
-  entity.price = event.params.price
+  // Save that event in our graph
+  // Update our activeitems
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  // Get or create an itemlisted object
+  // Each item needs an unique id
 }
 
-export function handleItemCanceled(event: ItemCanceledEvent): void {
-  let entity = new ItemCanceled(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.seller = event.params.seller
-  entity.nftAddress = event.params.nftAddress
-  entity.tokenId = event.params.tokenId
+export function handleItemCanceled(event: ItemCanceledEvent): void {}
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleItemListed(event: ItemListedEvent): void {
-  let entity = new ItemListed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.seller = event.params.seller
-  entity.nftAddress = event.params.nftAddress
-  entity.tokenId = event.params.tokenId
-  entity.price = event.params.price
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+export function handleItemListed(event: ItemListedEvent): void {}
